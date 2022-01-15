af35f66a968c25759d33ce23db3a456c66d7f393
yagomarialva <yago.marialva@gmail.com>
2022-01-15 12:37:38 -0400

    Adding deleted and update methods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List
-from uuid import uuid4
-from fastapi import FastAPI
-from models import Gender, Roles, User
+from uuid import UUID, uuid4
+from fastapi import FastAPI, HTTPException
+from models import Gender, Roles, User, UserUpdateRequest
 
 app = FastAPI()
 
@@ -24,6 +24,42 @@ db: List[User] = [
 async def root():
     return {"Hello": "World"}
 
+
 @app.get("/api/v1/users")
 async def fetch_users():
-    return db;
+    return db
+
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists")
+
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+        raise HTTPException(
+            status_code=404,
+            detail=f"user with id: {user_id} does not exists")
